ed129961f12f900ce34dc94a366e838816d78ad2
Jiani Cao <jianicakes@gmail.com>
2024-09-16 18:15:57 -0700

    respond with body

--- app/main.js
+++ app/main.js
@@ -1,23 +1,27 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-
-// Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const requestLine = data.toString().split("\n")[0];
-    const [method, target, version] = requestLine.trim().split(" ");
+    const [method, path, version] = requestLine.trim().split(" ");
 
-    if (target === "/") {
+    if (path === "/") {
       socket.write("HTTP/1.1 200 OK \r\n\r\n");
+    } else if (path.startsWith("/echo/")) {
+      const content = path.replace("/echo/", "");
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`
+      );
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
   });
+
   socket.on("close", () => {
     socket.end();
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  console.log("Server started at http://localhost:4221 ✧♡(◕‿◕✿)");
+});
