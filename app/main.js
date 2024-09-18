62182d959a36e4b648c41fa90d37345d099b2317
Jiani Cao <jianicakes@gmail.com>
2024-09-17 17:18:37 -0700

    read user-agent req header

--- app/main.js
+++ app/main.js
@@ -2,11 +2,19 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const requestLine = data.toString().split("\n")[0];
+    const [requestLine, ...otherLines] = data.toString().split("\r\n");
     const [method, path, version] = requestLine.trim().split(" ");
+    const headers = Object.fromEntries(
+      otherLines.filter((el) => el).map((pair) => pair.split(": "))
+    );
 
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK \r\n\r\n");
+    } else if (path === "/user-agent") {
+      const userAgent = headers["User-Agent"];
+      socket.write(
+        `HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+      );
     } else if (path.startsWith("/echo/")) {
       const content = path.replace("/echo/", "");
       socket.write(
